207b2a6a44e0910ff0e6e333a1b1ca6587295386
Jake Wyatt <Jake Wyatt>
2017-07-07 23:50:06 -0500

    stock check working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -22,15 +22,40 @@ connection.connect(function(err) {
 //--Query Test--//
 connection.query("SELECT * FROM products", function(err, res) {
 	if (err) throw err;
-	//console.log(res);
+  //console.log(res);
 });
 
-//--Call Function on Load--//
+//--Application--//
 
-bamazonConfirm();
+//--Run Welcome Screen with Available Products--//
+storeLoad();
 
-function bamazonConfirm () {
 
+//******Function Libaries********//
+
+//--Query from DB for Product Listing--//
+function selector() {
+  connection.query("SELECT * FROM products", function(err, res) {
+    for (var i = 0; i < res.length; i++) {
+      console.log("Item ID = " + res[i].item_id + " | " + res[i].product_name + " | " + "Department - " 
+        + res[i].department_name + " | " + "Price $ = " + res[i].price);
+    }
+    console.log("-----------------------------------");
+  });
+};
+
+//--Welcome Screen Function--//
+function storeLoad() {
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+    console.log("Welcome to the store! Below you can see what we have available for purchase!");
+      selector();
+      setTimeout(bamazonConfirm, 1500);
+    });
+};
+
+//--Place Order Function--//
+function bamazonConfirm() {
   inquirer.prompt([
   {
     type:"confirm",
@@ -39,39 +64,43 @@ function bamazonConfirm () {
   
   }]).then(function(answers){
     if (answers.confirm) {
-          selector();
           bamazonBuy();
     } else {
-          console.log("I said good day to you sir!");
+          console.log("Thank you for visiting!!");
           };
   });
 };
 
-
+//-- Buying of Product Function--//
 function bamazonBuy() {
   inquirer.prompt([
   {
       type:"input",
-      message:"What is the ID of the product you would like to buy? \n",
+      message:"What is the product ID you would like to buy? \n",
       name:"idchoice"
   },
   {
     type:"input",
-      message:"How many units of the product would you like to buy? \n",
+      message:"How many would you like to purchase? \n",
       name:"quantity"
   }
       ]).then(function(answers){
-        console.log(answers.idchoice);
-        console.log(answers.quantity);
+        var queryItem = "SELECT * FROM products WHERE item_id = ?";
+          connection.query(queryItem, [answers.idchoice], function(err, res) {
+            if (err) throw err;
+            //console.log(res[0].stock_quantity);
+              if (parseFloat(answers.quantity) <= parseFloat(res[0].stock_quantity)) {
+                console.log("Congrats, We have Stock on Hand for your order!");
+              } 
+              else {
+                console.log("Insufficient quantity!");
+              }
+          });
       });
 };
 
-
-function selector() {
-	connection.query("SELECT * FROM products", function(err, res) {
-	  for (var i = 0; i < res.length; i++) {
-	    console.log("Item ID = " + res[i].item_id + " | " + res[i].product_name + " | " + "Department - " + res[i].department_name + " | " + "Price $ = " + res[i].price);
-	  }
-	  console.log("-----------------------------------");
-	});
-};
+//--Stock Quantity Update Query Function--//
+// function queryUpdateStock() {
+//   var queryStock = "UPDATE bamazon SET ? WHERE item_id = ?";
+//   connection.query(queryStock, [answers.] function(err, res) {
+// }
